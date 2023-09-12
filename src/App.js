eac977da752390153f8dfc44012eb2142c3ef78a
emmanuel <segunbolawole@yahoo.com>
2023-09-12 07:21:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" index element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import ReactCardFlip from 'react-card-flip'
+// import { getDocs, collection } from 'firebase/firestore'
+// import { db } from '../../firebase'
+
+const portfolio = [
+  {
+    id: 100,
+    image:
+      'https://res.cloudinary.com/djt6ixhup/image/upload/v1694305675/mobile-app_yg6864.png',
+    name: 'Ecommerce Web App',
+    description:
+      'This App pulls its uniquness from the ability to connect to the inventory of the supermarket',
+    url: 'https://github.com/segunbol',
+  },
+  {
+    id: 200,
+    image:
+      'https://res.cloudinary.com/djt6ixhup/image/upload/v1673398546/samples/food/fish-vegetables.jpg',
+    name: 'Ecommerce Mobile App',
+    description:
+      'This App pulls its uniquness from the ability to connect to the inventory of the supermarket',
+    url: 'https://github.com/segunbol',
+  },
+  {
+    id: 300,
+    image:
+      'https://res.cloudinary.com/djt6ixhup/image/upload/v1673398546/samples/animals/reindeer.jpg',
+    name: 'Study Buddy Web App',
+    description:
+      'This app was created to connect student across the globe for the purpose of research, learning and ideation',
+    url: 'https://github.com/segunbol',
+  },
+  {
+    id: 400,
+    image:
+      'https://res.cloudinary.com/djt6ixhup/image/upload/v1673398544/samples/ecommerce/analog-classic.jpg',
+    name: 'Chat App',
+    description:
+      'This is an in-app chat, that enables customers to easily communicate with vendors',
+    url: 'https://github.com/segunbol',
+  },
+]
+const Portfolio = () => {
+  const [flip, setFlip] = useState(false);
+  const [letterClass, setLetterClass] = useState('text-animate')
+  //   const [portfolio, setPortfolio] = useState([])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  })
+
+  //   useEffect(() => {
+  //     getPortfolio()
+  //   }, [])
+
+  //   const getPortfolio = async () => {
+  //     const querySnapshot = await getDocs(collection(db, 'portfolio'))
+  //     setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
+  //   }
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      
+        <div className="images-container">
+          {portfolio.map((port, idx) => {
+            return (
+              <ReactCardFlip isFlipped={flip} flipDirection="horizontal" key={port.id}> 
+                <div className="image-box" >
+                  <img
+                    src={port.image}
+                    className="portfolio-image"
+                    alt="portfolio"
+                  />
+                  <div className="content">
+                    <p className="title">{port.name}</p>
+                    <h4 className="description">{port.description}</h4>
+                    <button className="btn" onClick={() => setFlip(!flip)}>
+                      View
+                    </button>
+                  </div>
+                </div>
+                
+                  <div className="content" key={idx}>
+                    <p className="title">{port.name}</p>
+                    <h4 className="description">{port.description}</h4>
+                    <button className="btn" onClick={() => setFlip(!flip)}>
+                      View
+                    </button>
+                  
+                </div>
+              </ReactCardFlip>  
+            )
+          })}
+        </div>
+      
+    )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Portfolio
+// onClick={() => window.open(port.url)}
